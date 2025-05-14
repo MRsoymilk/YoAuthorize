1143d65c73c26ecdfcea3c1e7a9f44620400c4f1
MRsoymilk <codermrsoymilk@gmail.com>
2025-05-14 18:19:38 +0800

    feat(gpu): get basic GPU information

--- src/ya_hwinfo/gpu/yagpu.cpp
+++ src/ya_hwinfo/gpu/yagpu.cpp
@@ -1,1 +1,60 @@
 #include "yagpu.h"
+
+#include <filesystem>
+#include <fstream>
+
+#include "platform_def.h"
+
+YaGPU::YaGPU() { init(); }
+
+YaGPU::~YaGPU() {}
+
+std::string YaGPU::getName() { return m_gpu.name; }
+
+std::string YaGPU::getSerialNumber() { return m_gpu.serial_number; }
+
+std::string YaGPU::getManufacturer() { return m_gpu.manufacturer; }
+
+void YaGPU::init() {
+#if defined(YA_LINUX)
+  namespace fs = std::filesystem;
+  std::string base_path = "/sys/class/drm";
+
+  auto trim = [](const std::string& str) -> std::string {
+    const auto begin = str.find_first_not_of(" \t\n\r");
+    const auto end = str.find_last_not_of(" \t\n\r");
+    if (begin == std::string::npos || end == std::string::npos) return "";
+    return str.substr(begin, end - begin + 1);
+  };
+
+  auto vendor_id_to_name = [](const std::string& id) -> std::string {
+    if (id == "0x10de") return "NVIDIA";
+    if (id == "0x1002") return "AMD";
+    if (id == "0x8086") return "Intel";
+    return "";
+  };
+
+  for (const auto& entry : fs::directory_iterator(base_path)) {
+    const std::string name = entry.path().filename().string();
+
+    if (name.find("card") == 0 &&
+        fs::exists(entry.path() / "device" / "vendor")) {
+      std::ifstream vendor_file(entry.path() / "device" / "vendor");
+      std::ifstream device_file(entry.path() / "device" / "device");
+
+      std::string vendor_id, device_id;
+      std::getline(vendor_file, vendor_id);
+      std::getline(device_file, device_id);
+
+      vendor_id = trim(vendor_id);
+      device_id = trim(device_id);
+
+      m_gpu.manufacturer = vendor_id_to_name(vendor_id);
+      m_gpu.serial_number = device_id;
+      m_gpu.name = name;
+
+      break;  // TODO: multi GPU
+    }
+  }
+#endif
+}
--- src/ya_hwinfo/gpu/yagpu.h
+++ src/ya_hwinfo/gpu/yagpu.h
@@ -1,8 +1,21 @@
 #ifndef YA_GPU_H
 #define YA_GPU_H
+
 #include "info_def.h"
+
 class YaGPU {
  public:
+  YaGPU();
+  ~YaGPU();
+  std::string getName();
+  std::string getSerialNumber();
+  std::string getManufacturer();
+
+ private:
+  void init();
+
+ private:
+  GPU m_gpu;
 };
 
 #endif  // !YA_GPU_H
